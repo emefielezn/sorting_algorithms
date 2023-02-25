f3685f47ae2483c99b9b0143ab8f0a7194f818dc
emefielezn <emefielengozi54@gmail.com>
2023-02-24 18:26:40 -0800

    changes to code

--- deck.h
+++ deck.h
@@ -1,5 +1,5 @@
 #ifndef DECK_H
-#define DECK_h
+#define DECK_H
 /*structs*/
 /**
  * enum kind_e - card suit
@@ -11,9 +11,9 @@
 typedef enum kind_e
 {
 	SPADE = 0;
-	HEART,
-	CLUB,
-	DIAMOND,
+	HEART;
+	CLUB;
+	DIAMOND;
 } kind_t;
 /**
  * struct card_s - playing card
@@ -24,7 +24,7 @@ typedef struct card_s
 {
 	const char *value;
 	const kind_t kind;
-} card_t
+} card_t;
 /**
  * struct deck_node_s - deck of card
  * @card: pointer to the card of the node
@@ -36,7 +36,7 @@ typedef struct deck_node_s
 	const card_t *card;
 	struct deck_node_s *prev;
 	struct deck_node_s *next;
-} deck_node-t;
+} deck_node_t;
 /*prototype*/
 void sort_deck(deck_node_t **deck);
 #endif
